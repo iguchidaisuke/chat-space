185b8634b2f5cff36e14702ed08ba9bea0d8bb33
iguchidaisuke <59217782+iguchidaisuke@users.noreply.github.com>
2020-01-08 20:18:41 +0900

    new group

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only:[:edit, :update]
-  root "messages#index"
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
+  root "groups#index"
+  
 end
