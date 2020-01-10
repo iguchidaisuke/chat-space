d88b9507ec2d82ef5ce9d3a5428f271b90af8bad
iguchidaisuke <59217782+iguchidaisuke@users.noreply.github.com>
2020-01-10 14:53:33 +0900

    メッセージ機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only:[:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+  resources :users, only:[:edit, :update,:edit]
+  resources :groups, only: [:new, :create, :edit, :update,:edit] do
+    resources :messages, only: [:index, :create, :edit]
   end
   root "groups#index"
   
