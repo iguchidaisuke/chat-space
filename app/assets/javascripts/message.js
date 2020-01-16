ed276893382eb8ae8e94271980e6a3b6cea47e61
iguchidaisuke <59217782+iguchidaisuke@users.noreply.github.com>
2020-01-16 14:32:35 +0900

    upload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,46 +1,24 @@
 $(function(){ 
-  function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="main_chat_message" data-message-id=${message.id}>
+  
+  var buildHTML = function(message){
+
+    var img = message.image ? `<img src= ${message.image}>` : "";
+
+    var html =`<div class="main_chat_message" data-message-id="${message.id}">
          <div class="main_chat_message_info">
-           <div class="main_chat_message_info__talker">
-             ${message.user_name}
-           </div>
-           <div class="main_chat_message_info__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="main_chat_meesage_text">
-           <p class="main_chat_meesage_text_content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
+           <div class="main_chat_message_info__talker">${message.user_name}</div>
+           <div class="main_chat_message_info__date">${message.created_at}</div>
+          </div>
+          <div class="main_chat_meesage_text">
+            <p class="main_chat_meesage_text_content">${message.content}
+            </p>
+        </div>
+        ${img}
+      </div>`
      return html;
-   } else {
-     var html =
-      `<div class="main_chat_message" data-message-id=${message.id}>
-         <div class="main_chat_message_info">
-           <div class="main_chat_message_info__talker">
-             ${message.user_name}
-           </div>
-           <div class="main_chat_message_info__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="main_chat_meesage_tex">
-           <p class="main_chat_meesage_text_content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
- 
-$('#new_message').on('submit', function(e){
+  }
+
+  $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action')
@@ -53,7 +31,6 @@ $('#new_message').on('submit', function(e){
    processData: false,
    contentType: false
  })
-
   .done(function(data){
     var html = buildHTML(data);
     $('.message').append(html);
@@ -64,4 +41,33 @@ $('#new_message').on('submit', function(e){
     alert("メッセージ送信に失敗しました");
   });
  })
+
+var reloadMessages = function() {
+  last_message_id = $('.main_chat_message:last').data("message-id");
+  console.log(last_message_id);
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.message').append(insertHTML);
+      $('.message').animate({ scrollTop: $('.message')[0].scrollHeight});
+      $("#new_message")[0].reset();
+      $(".form__submit").prop("disabled", false);
+    }
+  })
+  .fail(function() {
+  });
+};
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+}
 });
